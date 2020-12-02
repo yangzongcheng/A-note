af04da755dd24268651f2a12b0c2d7e7ed656bf4
yangzongchegng <1432704394@qq.com>
2020-12-02 10:11:10 +0800

    sd

--- 数据库/mysql/锁/lock.sql
+++ 数据库/mysql/锁/lock.sql
@@ -8,7 +8,7 @@ INSERT INTO t_user VALUE(null,'李四');
 
 -- MyISAM存储引擎只支持表锁
 
--- 针对 MyISAM
+-- 针对 MyISAM（innodb 也能加）
 lock tables t_order read; -- 读锁 只能查询 不能insert  update delete
 -- 如果表被加上了读锁 执行写操作会提示错误
 select * from t_order;
@@ -26,8 +26,9 @@ UNLOCK tables;-- 解锁
 
 
 -- 行锁 innodb    行锁分为共享锁（读锁）和排他锁（写锁）
--- 1、共享锁又叫做读锁，所有的事务只能对其进行读操作不能写操作，加上共享锁后在事务结束之前其他事务只能再加共享锁，除此之外其他任何类型的锁都不能再加了
---     用法：SELECT `id` FROM  table WHERE　id in(1,2) LOCK IN SHARE MODE 结果集的数据都会加共享锁
+-- 1、共享锁又叫做读锁，
+--  所有的事务只能对其进行读操作不能写操作，加上共享锁后在事务结束之前其他事务只能再加共享锁，除此之外其他任何类型的锁都不能再加了
+--  用法：SELECT `id` FROM  table WHERE　id in(1,2) LOCK IN SHARE MODE 结果集的数据都会加共享锁
 
 -- 2、若某个事物对某一行加上了排他锁，只能这个事务对其进行读写，在此事务结束之前，其他事务不能对其进行加任何锁，其他进程可以读取,不能进行写操作，需等待其释放。
 -- SELECT `id` FROM mk_user WHERE id=1 FOR UPDATE 注意：由于在innodb中行锁，他是针对索引去锁定该条数据，而不是直接锁定该条数据的。
