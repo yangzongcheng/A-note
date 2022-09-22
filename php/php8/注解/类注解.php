1d174f825ef8222cc407aa2402495ac5b23ef5c6
yangzongchegng <1432704394@qq.com>
2022-09-22 14:43:11 +0800

    2022年09月22日14:43:09

---
+++ php/php8/注解/类注解.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * 注解类
+ * DateTime:  2022/9/22 14:15:06
+ * ClassName: Myattr
+ */
+#[Attribute(Attribute::TARGET_CLASS)]
+class Myattr{
+    public string $name;
+    public mixed $value;
+    public int $number;
+
+    public function __construct($name,$value,$number){
+        $this->name=$name;
+        $this->value=$value;
+        $this->number=$number;
+    }
+
+    public function say(){
+        echo "{$this->name}--{$this->value}  --  {$this->number}";
+    }
+}
+
+
+/**
+ * 抽象
+ * DateTime:  2022/9/22 14:26:44
+ * ClassName: DataAbstract
+ */
+abstract class DataAbstract
+{
+    public function getData():array
+    {
+        #ReflectionFunction 函数注解
+        $ref=new ReflectionClass(static::class);
+        $attr=$ref->getAttributes(Myattr::class)[0];
+        $obj=$attr->newInstance();
+        $arr =[
+            'name'=>$obj->name,
+            'value'=>$obj->value,
+            'number'=>$obj->number
+        ];
+        return $arr;
+    }
+}
+
+
+#[Myattr('A','A类注解',000)]
+class A extends DataAbstract {
+
+}
+
+#[Myattr('B','B类注解',12333123)]
+class B extends DataAbstract{
+
+}
+
+
+$data = (new A())->getData();
+
+
+print_r($data);
+
