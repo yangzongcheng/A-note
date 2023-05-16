2019964f2e9fa53f47a6fc203463a5be8b25befa
yangzongchegng <1432704394@qq.com>
2023-05-16 15:42:04 +0800

    2023年05月16日15:42:02

--- 数据库/redis/Lock分布式锁.php
+++ 数据库/redis/Lock分布式锁.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Service\BigData\DataGovernance\EtlTask\Logic\Task;
 
 use Provider\Helper;
 use Provider\Timer;
@@ -11,9 +12,9 @@ use Zxedu\Particle\Error\NotExistsError;
  * ClassName: RunLock
  * @package Service\BigData\DataGovernance\EtlTask\Logic\Task
  */
-class Lock分布式锁
+class RunLock
 {
-    private static int $expire = 3600;#默认3600秒过期
+    private static int $expire = 1800;#默认3600秒过期
     private static string $lockKey ='etl_task_run_handle_lock_';
 
     /**
@@ -31,7 +32,7 @@ class Lock分布式锁
         $lock = $redis->setnx($lockKey,$lockVale);
         if(!$lock && $redis->get($lockKey) > time())
         {
-            throw new NotExistsError('次任务还在执行中,时间:'.Timer::dateTo(time()));
+            throw new NotExistsError('任务在执行中,时间:'.Timer::dateTo(time()));
         }
         #执行到此处有两种情况 1senx执行获取锁成功 2key存在，但是值已经过期
 
@@ -39,9 +40,15 @@ class Lock分布式锁
         #如果出现并发获取锁的情况此步骤可以防止锁被同时获取
         #如 当锁释放失败的时候 两个进程同时获取到锁此时先获取到锁的会把value重新赋值
         #第二个进程在获取的到的时候值已经是1进程赋值的value了此时获取锁将失败
-        if($redis->getSet($lockKey,$lockVale)>time())
+        if(!$lock)
         {
-            throw new NotExistsError('次任务还在执行中,时间:'.Timer::dateTo(time()));
+            $oldVal = $redis->getSet($lockKey,$lockVale);
+            if($oldVal > time())
+            {
+                #把值改回去
+                $redis->set($lockKey,$oldVal);
+                throw new NotExistsError('任务还在执行中,时间:'.Timer::dateTo(time()));
+            }
         }
         #获取锁成功,设置过期时间
         $redis->expire($lockKey,self::$expire);
